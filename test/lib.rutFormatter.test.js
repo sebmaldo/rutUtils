ca06d443d7899e47608411ba295bc1d97dfa4c8e
Sebastián Maldonado <sebastian@becual.com>
2017-02-06 10:39:36 -0300

    Cambio de nombre de archivo en librería y modificación para exportar solo función

--- test/lib.rutFormatter.test.js
+++ test/lib.rutFormatter.test.js
@@ -1,23 +1,17 @@
 import test from 'ava';
-import rutFormatter from '../lib/rutFormatter';
+import rf from '../lib/formatoRut';
 
 test('Return must be equal to `15.649.137-3`', t => {
-
-    let result = rutFormatter.formatRUT('156491373');
-
+    let result = rf('156491373');
     t.is(result, '15.649.137-3');
-
 });
 
 test('Return must be equal to `9.457.904-K`', t => {
-
-    let result = rutFormatter.formatRUT('9457904k');
-
+    let result = rf('9457904k');
     t.is(result, '9.457.904-K');
-
 });
 
 test('Return must be equal to `1`', t => {
-    let result = rutFormatter.formatRUT('1');
+    let result = rf('1');
     t.is(result, '1');
 });
